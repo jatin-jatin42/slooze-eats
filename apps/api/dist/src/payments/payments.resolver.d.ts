75e7de5a19726f487809d934faca90c47d656d3f
jatin-sdev <jatin.sdev@gmail.com>
2026-03-15 22:26:10 +0530

    chore: stop tracking compiled files in apps/api/dist

--- apps/api/dist/src/payments/payments.resolver.d.ts
+++
@@ -1,9 +0,0 @@
-import { PaymentsService } from './payments.service';
-export declare class PaymentsResolver {
-    private paymentsService;
-    constructor(paymentsService: PaymentsService);
-    paymentMethods(context: any): Promise<string>;
-    addPaymentMethod(userId: string, type: string, last4: string, name: string, context: any): Promise<string>;
-    updatePaymentMethod(id: string, dataJson: string, context: any): Promise<string>;
-    deletePaymentMethod(id: string, context: any): Promise<string>;
-}
--- apps/api/dist/src/restaurants/restaurants.resolver.d.ts
+++
@@ -1,8 +0,0 @@
-import { RestaurantsService } from './restaurants.service';
-export declare class RestaurantsResolver {
-    private restaurantsService;
-    constructor(restaurantsService: RestaurantsService);
-    restaurants(context: any): Promise<string>;
-    restaurant(id: string, context: any): Promise<string>;
-    menuItems(restaurantId: string, context: any): Promise<string>;
-}
